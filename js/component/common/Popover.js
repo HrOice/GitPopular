23e69405e2d85edc04b2d3c4c68a3cac452ff4ca
栾博文 <lbw@terminus.io>
2017-05-18 09:28:57 +0800

    save

---
+++ js/component/common/Popover.js
@@ -0,0 +1,29 @@
+import React, { PropTypes } from 'react';
+import {
+  StyleSheet,
+  Dimensions,
+  Animated,
+  Text,
+  TouchableWithoutFeedback,
+  View,
+  Easing
+} from 'react-native';
+
+
+export default class Popover extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        <View style={{flex: 1, paddingTop: 20}}>
+            <Animated.View>
+
+            </Animated.View>
+            <Text>click me</Text>
+        </View>
+    }
+}
